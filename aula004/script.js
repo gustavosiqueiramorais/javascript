d72f71bf1150fdc8638ed8c282ea7457c83f5325
Gustavo Siqueira <siqueiragustavocontato@gmail.com>
2025-07-23 12:10:47 -0300

    Atualização

---
+++ aula004/script.js
@@ -0,0 +1,29 @@
+var num1 = Number.parseInt(window.prompt('Digite um numero inteiro'));
+var num2 = Number.parseInt(window.prompt('Digite outro numero'));
+var soma = num1 + num2;
+window.alert(`A soma entre esses dois números é igual a ${soma}`);
+
+var num3 = Number.parseInt(window.prompt('Digite um numero'));
+var num4 = Number.parseInt(window.prompt('Digite outro numero'));
+var subtração = num3 - num4;
+window.alert(`A subtração entre esses dois numeros é igual a ${subtração}`);
+
+var num5 = Number.parseInt(window.prompt('Digite um numero inteiro'));
+var num6 = Number.parseInt(window.prompt('Digite mais um numero'));
+var multiplicação = num5 * num6;
+window.alert(`A multiplicação entre esses numeros é = ${multiplicação}`);
+
+var num7 = Number.parseInt(window.prompt('Digite um numero inteiro'));
+var num8 = Number.parseInt(window.prompt('Digite mais um numero inteiro'));
+var divisão = num7 / num8;
+window.alert(`A divisão entre esses dois numeros é = ${divisão}`);
+
+var num9 = Number.parseInt(window.prompt('Digite um numero'));
+var num10 = Number.parseInt(window.prompt('Digite outro numero'));
+var resto = num9 % num10;
+window.alert(`O resto da divisão entre esses dois numeros é = ${resto}`)
+
+var num11 = Number(window.prompt('Digite um numero inteiro'));
+var num12 = Number(window.prompt('Digite outro numero'));
+var potencia = num11 ** num12;
+window.alert(`O resultado dessa potenciação é = ${potencia}`);
