f33243ae315ef9b4afefe38b3c81e53aae73dc38
gustavosiqueiramorais <siqueiragustavocontato@gmail.com>
2026-03-13 11:34:45 -0300

    Formatando data para formato local

--- obtendoDataJs/script.js
+++ obtendoDataJs/script.js
@@ -8,3 +8,15 @@ let hora = data.getHours();
 let minutos = data.getMinutes();
 
 console.log(`${dia}/${mes}/${ano} ás ${hora}:${minutos}`);
+
+
+console.log(data.toString());
+
+console.log(data.toDateString());
+
+console.log(data.toTimeString());
+
+console.log(data.toLocaleDateString());
+console.log(data.toLocaleTimeString());
+
+
