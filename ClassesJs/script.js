2d5af646e63010fb48cde4d9cd7facee39a12429
gustavosiqueiramorais <siqueiragustavocontato@gmail.com>
2026-03-18 11:30:55 -0300

    Criando e accessando propriedades e métodos

--- ClassesJs/script.js
+++ ClassesJs/script.js
@@ -6,3 +6,30 @@ class sayajin{
 
 const gohan = new sayajin();
 
+//Criando e acessando propiedades
+class cachorro{
+    constructor(name){
+        this.nome = this.nome; //Criei a propriedade
+    }
+}
+
+let cachorro1 = new cachorro("Major"); //Criei um objeto e passei um parametro a classe
+
+console.log(cachorro1.nome); //Acessei a propriedade nome do objeto cachorro1
+
+
+//Criando e accesando métodos
+
+class boxeador{
+    constructor(name){
+        this.name = name;
+    }
+
+    porrada(){ //Criando o método
+        console.log("POWWWWW");
+    }
+}
+
+let boxeador1 = new boxeador("Myke");
+
+boxeador1.porrada();//Acessando o método
